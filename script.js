bf4ae1d680e81580ffa30ac161767da3e3fa72c0
wcam <campbell.programming@gmail.com>
2016-10-05 11:42:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+﻿$('span').click(function() 
+{
+    console.log("You clicked it LOL");
+
+    $('body').css('background-color', 'yellow');
+});
+
+$('body').css
+({
+  'margin-top':'5%',
+  'margin-bottom':'5%',
+  'margin-left':'10%',
+  'margin-right':'10%'
+  });
+
+$('body').css('background-color', '#FFFFFF');
